a55c44ae6a09c20767fef7600ebb7c1e2b7880b8
LCano <leonardomacedocano@gmail.com>
2020-07-19 21:41:21 -0300

    add setPropertyStyle function

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 var
 	bgColor;
 
+function setPropertyStyle(id, valor) {
+	document.body.style.setProperty(`--${id}`, valor);
+}
+
 function start() {
 	bgColor = '#ffa07a';
+	setPropertyStyle('bgColor', bgColor);
 }
 
 start();
