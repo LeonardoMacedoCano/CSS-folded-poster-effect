9d5ad090562e6b03fc23eaf57b2711e050ba84c7
LCano <leonardomacedocano@gmail.com>
2020-07-19 23:28:05 -0300

    add function to show and hide the menu

--- main.js
+++ main.js
@@ -1,6 +1,25 @@
 var
 	bgColor;
 
+$(document).ready(function(e){
+	var windowHeight = window.innerHeight;
+	$('#navigation-menu').css('height', windowHeight + 'px');
+
+	$(document).on('click', '#showMenu', function(){
+		$('#navigation-menu').css('left', '0');
+		$('#showMenu, #hideMenu').css('left', '160px');
+		$('#showMenu, #hideMenu').html('<i class="fa fa-chevron-left"></i>');
+		$('#showMenu').attr('id', 'hideMenu');
+	});
+
+	$(document).on('click', '#hideMenu', function(){
+		$('#navigation-menu').css('left', '-160px');
+		$('#showMenu, #hideMenu').css('left', '0');
+		$('#showMenu, #hideMenu').html('<i class="fa fa-cog"></i>');
+		$('#hideMenu').attr('id', 'showMenu');
+	});
+});
+
 function setPropertyStyle(id, valor) {
 	document.body.style.setProperty(`--${id}`, valor);
 }
